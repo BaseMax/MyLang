9557d8a5a837134d0e746313019c3e60d7fceacf
Max Base <MaxBaseCode@gmail.com>
2021-05-03 17:00:30 +0430

    Solve problem of grammar file /create ast output file beautify

--- mylang.js
+++ mylang.js
@@ -5,9 +5,24 @@ function id(x) { return x[0]; }
 var grammar = {
     Lexer: undefined,
     ParserRules: [
-    {"name": "program", "symbols": ["var_assignment"], "postprocess": id},
-    {"name": "program", "symbols": ["print_statement"], "postprocess": id},
-    {"name": "program", "symbols": ["number"], "postprocess": id},
+    {"name": "program", "symbols": ["statements"], "postprocess": id},
+    {"name": "statements", "symbols": ["statement"], "postprocess": id},
+    {"name": "statements", "symbols": ["statement", {"literal":"\n"}, "statements"], "postprocess": 
+        data => [data[0], ...data[2]]
+        	},
+    {"name": "statement", "symbols": ["var_assignment"], "postprocess": id},
+    {"name": "statement", "symbols": ["print_statement"], "postprocess": id},
+    {"name": "statement", "symbols": ["while_loop"], "postprocess": id},
+    {"name": "while_loop$string$1", "symbols": [{"literal":"w"}, {"literal":"h"}, {"literal":"i"}, {"literal":"l"}, {"literal":"e"}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "while_loop", "symbols": ["while_loop$string$1", "__", "binary_expression", "__", {"literal":"["}, "_", {"literal":"\n"}, "statements", {"literal":"\n"}, {"literal":"]"}], "postprocess": 
+        data => {
+        	return {
+        		type: "while_loop",
+        		condition: data[2],
+        		body: data[7]
+        	}
+        }
+        	},
     {"name": "print_statement$string$1", "symbols": [{"literal":"p"}, {"literal":"r"}, {"literal":"i"}, {"literal":"n"}, {"literal":"t"}], "postprocess": function joiner(d) {return d.join('');}},
     {"name": "print_statement", "symbols": ["print_statement$string$1", "__", "number"], "postprocess": 
         data => {
@@ -25,26 +40,32 @@ var grammar = {
     {"name": "operator", "symbols": [{"literal":"-"}], "postprocess": id},
     {"name": "operator", "symbols": [{"literal":"*"}], "postprocess": id},
     {"name": "operator", "symbols": [{"literal":"/"}], "postprocess": id},
+    {"name": "operator$string$1", "symbols": [{"literal":">"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$1"], "postprocess": id},
+    {"name": "operator$string$2", "symbols": [{"literal":"<"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "operator", "symbols": ["operator$string$2"], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":">"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"<"}], "postprocess": id},
+    {"name": "operator", "symbols": [{"literal":"="}], "postprocess": id},
     {"name": "binary_expression", "symbols": ["unary_expression", "_", "operator", "_", "expression"], "postprocess": 
-        		data => {
-        			return {
-        				type: "binary_expression",
-        				left: data[0],
-        				operator: data[2],
-        				right: data[4],
-        			}
-        		}
+        data => {
+        	return {
+        		type: "binary_expression",
+        		left: data[0],
+        		operator: data[2],
+        		right: data[4],
         	}
-        
-        var_assignment
-        	-> identifier _ ":=" _ expression {%
-        		data => {
-        			return {
-        				type: "var_assignment",
-        				varname: data[0],
-        				value: data[2]
-        			}
-        		}
+        }
+        	},
+    {"name": "var_assignment$string$1", "symbols": [{"literal":":"}, {"literal":"="}], "postprocess": function joiner(d) {return d.join('');}},
+    {"name": "var_assignment", "symbols": ["identifier", "_", "var_assignment$string$1", "_", "expression"], "postprocess": 
+        data => {
+        	return {
+        		type: "var_assignment",
+        		varname: data[0],
+        		value: data[2]
+        	}
+        }
         	},
     {"name": "identifier$ebnf$1", "symbols": [/[a-z]/]},
     {"name": "identifier$ebnf$1", "symbols": ["identifier$ebnf$1", /[a-z]/], "postprocess": function arrpush(d) {return d[0].concat([d[1]]);}},
--- parser.js
+++ parser.js
@@ -1,11 +1,26 @@
 const nearley = require("nearley")
 const grammar = require("./mylang")
+const fs = require("mz/fs")
+const path = require("path")
 
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
+async function main() {
 
-try {
-	parser.feed("a:=3.14")
-	console.log(parser.results)
-} catch(e) {
-	console.log(`Parse failed: ${e.message}`)
+	const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar))
+	const filename = process.argv[2]
+	const outputFilename = path.basename(filename, ".mylang") + ".ast"
+	const code = (await fs.readFile(filename)).toString()
+
+	try {
+		// parser.feed("a:=3.14")
+		parser.feed(code)
+
+		const ast = parser.results[0]
+		await fs.writeFile(outputFilename, JSON.stringify(ast, null, 2))
+
+		console.log("Parse succeeded.")
+		console.log(parser.results)
+	} catch(e) {
+		console.log(`Parse failed: ${e.message}`)
+	}
 }
+main()
